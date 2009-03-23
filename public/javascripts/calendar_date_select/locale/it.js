89f767b850a0b5b03c2c0809d7d372f42400c12c
Bigo <bigonazzi@gmail.com>
2009-03-23 22:38:20 +0100

    Added italian translations (:locale => 'it')

---
+++ public/javascripts/calendar_date_select/locale/it.js
@@ -0,0 +1,10 @@
+Date.weekdays = $w("D L M M G V S");
+Date.months = $w("Gennaio Febbraio Marzo Aprile Maggio Giugno Luglio Agosto Settembre Ottobre Novembre Dicembre");
+
+Date.first_day_of_week = 1;
+
+_translations = {
+  "OK": "OK",
+  "Now": "Ora",
+  "Today": "Oggi"
+};
